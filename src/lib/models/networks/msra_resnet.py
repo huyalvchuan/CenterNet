66ff00b618919f04b4f30ce9d37b7b11727b8c1d
huyalvchuan <2014689070@qq.com>
2019-09-10 17:06:39 +0000

    convert

--- src/lib/models/networks/msra_resnet.py
+++ src/lib/models/networks/msra_resnet.py
@@ -221,10 +221,15 @@ class PoseResNet(nn.Module):
         x = self.layer4(x)
 
         x = self.deconv_layers(x)
-        ret = {}
+        # ret = {}
+        # for head in self.heads:
+        #     ret[head] = self.__getattr__(head)(x)
+        # return [ret]
+        ret = []
         for head in self.heads:
-            ret[head] = self.__getattr__(head)(x)
-        return [ret]
+            ret.append(self.__getattr__(head)(x))
+        return ret
+        
 
     def init_weights(self, num_layers, pretrained=True):
         if pretrained:
